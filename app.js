a32ac204967a90845ac90c5783a9cfe7d635a73d
manishdixit18 <manishdixit18@gmail.com>
2017-06-07 11:49:09 +0530

    ajax call and its response

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ var date=require('./getDateModule');
 var url="mongodb://localhost:27017/chat";
 var express = require('express')
 var app = express()
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8081');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
+    next();
+}
+app.use(allowCrossDomain);
+ 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
 	MongoClient.connect(url, function(err, db) {
@@ -23,24 +31,15 @@ app.get('/', function (req, res) {
 app.listen(4200, function () {
   console.log('Example app listening on port 4200!')
 })
-// http.createServer(function (req, res) {
-    // res.writeHead(200, {'Content-Type': 'text/html'});
-    // res.write("Getting data by function "+getData()+" \n on date "+date.myDateTime());
-    // res.end();
-// }).listen(4200);
-
-// function getData(){
-	// MongoClient.connect(url, function(err, db) {
-	  // if (err) throw err;
-	  // var query = {};
-	  // db.collection("QuestionCollection").find(query).toArray(function(err, result) {
-	    // if (err) throw err;
-	    // // result.forEach(function(value){
-	    // // 	console.log(value.Qes+" "+value.ans);
-	    // // })
-	    // console.log("in fun "+result[0].answer);
-	    // db.close();
-	    // return result[0].answer;
-	  // });
-	// });
-// }
+app.post('/getResponse', function (req, res) {
+	MongoClient.connect(url, function(err, db) {
+	  if (err) throw err;
+	  var query = {};
+	  db.collection("QuestionCollection").find(query).toArray(function(err, result) {
+	    if (err) throw err;
+	    res.send(result[0].Question)
+	    db.close();
+	   
+	  });
+	});
+})
